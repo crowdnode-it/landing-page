377fdac75f6b6c283a0f25226fe0d6e41453dae4
Bruno <ichigoplayer18@gmail.com>
2026-06-08 19:21:18 +0200

    changing the debug status

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_ID}', { send_page_view: false, debug_mode:true });
+              gtag('config', '${GA_ID}', { send_page_view: false});
             `}} />
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
